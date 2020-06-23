2ac8ecb0f72391dd99e4123e100b76d12f7ad010
Ghazal <ghazal.fouladfard@gmail.com>
2020-06-23 23:52:45 +0000

    add teachers and assistance_request tables with fake data

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -15,14 +15,14 @@
 -- end_date: The students' end date of the Bootcamp
 -- cohort_id: The id of the cohort that the student is enrolled in
 
-CREATE TABLE cohort (
+CREATE TABLE cohorts (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   start_date DATE,
   end_date DATE
 );
 
-CREATE TABLE student (
+CREATE TABLE students (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255),
